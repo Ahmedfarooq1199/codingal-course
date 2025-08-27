b786c5d83e819781292ef4e8d4f62b77fd08dea0
Ahmedfarooq1199 <qandeeltooba@gmail.com>
2025-08-27 15:22:08 +0500

    innitial commit

---
+++ M7/classwork/L1-A1.py
@@ -0,0 +1,19 @@
+# Printing a greeting message
+print("hello, world")
+
+# Printng my name
+print("My name is alex")
+
+# Printing my age
+print("I am 30 years old")
+
+# Printing my hobbies
+print("I love playing games")
+
+# Performing and printing a simple calculation
+print("The result of 1 + 2 is:",1 + 2)
+
+'''
+This is 
+a multi-line comment.
+'''
