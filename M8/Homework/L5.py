fdfdc43a7c8e0c6854760e0e0e29729eedfd5b84
Ahmedfarooq1199 <qandeeltooba@gmail.com>
2025-10-07 13:54:09 +0500

    innitial commit

---
+++ M8/Homework/L5.py
@@ -0,0 +1,15 @@
+
+
+
+fruits = ["apple", "banana", "mango"]
+print("List:", fruits)
+
+
+colors = ("red", "green", "blue")
+print("Tuple:", colors)
+
+nums = {1, 2, 3, 3}
+print("Set:", nums)
+
+student = {"name": "Ahmed", "age": 18}
+print("Dictionary:", student)
